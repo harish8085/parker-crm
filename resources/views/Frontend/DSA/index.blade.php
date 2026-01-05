1842b7f093dd30a753485e014ddd27742432c7ae
DESKTOP-TL2A2IM\Acer <mishragaurav101097@gmail.com>
2026-01-05 16:30:39 +0530

    styling changes

--- resources/views/Frontend/DSA/index.blade.php
+++ resources/views/Frontend/DSA/index.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="{{ asset('assets/css/custom-table.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/settlement.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/paginate.css') }}">
-
+<link rel="stylesheet" href="{{ asset('assets/css/commen.css') }}">
 <style>
     .date_range {
         display: none;
@@ -19,8 +19,11 @@
         <div class="btn-container">
             @if(auth()->user()->hasPermission('dsa-code','create'))
 
-            <button class="application-header-btn" data-bs-toggle="modal" data-bs-target="#createDSA">
+            <button class="application-header-btn add-bank-target-res1" data-bs-toggle="modal" data-bs-target="#createDSA">
                 <img class="application-header-icon" src="{{ asset('assets/images/add-table-icon.svg') }}">Add Bank Code
+            </button>
+            <button class="application-header-btn add-bank-target-res2" data-bs-toggle="modal" data-bs-target="#createDSA">
+                <img class="application-header-icon" src="{{ asset('assets/images/add-table-icon.svg') }}">
             </button>
             @endif
         </div>
@@ -32,7 +35,7 @@
             <div class="col-lg-4 mb-2">
                 <div class="bank-detail-inputs">
                     <label class="bank-input-label">Date Range</label>
-                    <select class="bank-detail-input form-select select" required name="date" id="date">
+                    <select class="bank-detail-input form-select select date-filter" required name="date" id="date">
                         <option value="" selected disabled></option>
                         <option value="custom">Custom</option>
                         <option value="today">Today</option>
