779c8e0b9921fea563c1f29fbb40440188512299
harishmahajan646@gmail.com <harishmahajan646@gmail.com>
2026-03-30 13:29:59 +0530

    Fix cannel user isuue

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -398,7 +398,7 @@ class AuthController extends Controller
                 $parentId = $employeeCode->id;
             }
         } else {
-            $parentId = $masterCode->user_id;
+            $parentId = '';
         }
 
         // Handle file uploads before transaction (file operations aren't transactional)
@@ -489,10 +489,12 @@ class AuthController extends Controller
                 $bankData->save();
 
                 //Save associate channel
+                if ($parentId) {
                 $associateChannel = ChannelUser::create([
-                    'channel_id' => $parentId,
-                    'associate_channel_id' => $user->id
-                ]);
+                        'channel_id' => $parentId,
+                        'associate_channel_id' => $user->id
+                    ]);
+                }
 
                 // Commit the transaction if everything succeeds
                 DB::commit();
